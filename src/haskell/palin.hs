04010c1813ab87c07337f121ef3c4e3af09240f5
dsouza <dsouza@bitforest.org>
2011-09-14 11:58:11 -0300

    ...

--- src/haskell/palin.hs
+++ src/haskell/palin.hs
@@ -1,54 +1,54 @@
-zbqhyr Znva jurer
+module Main where
 
-vzcbeg dhnyvsvrq Qngn.Yvfg       nf Y
-vzcbeg dhnyvsvrq Qngn.OlgrFgevat nf O
-vzcbeg dhnyvsvrq Pune            nf P
+import qualified Data.List       as L
+import qualified Data.ByteString as B
+import qualified Char            as C
 
--- * Gur fhz nytbevguz gb n onfr10 rkcnafvba
-(<+>) :: [Vag] -> Vag -> [Vag]
-(<+>) [] a    = [a]
-(<+>) (k:kf) a 
-  | a+k>9     = 0 : kf <+> 1
-  | bgurejvfr = k+a : kf
+-- * The sum algorithm to a base10 expansion
+(<+>) :: [Int] -> Int -> [Int]
+(<+>) [] n    = [n]
+(<+>) (x:xs) n 
+  | n+x>9     = 0 : xs <+> 1
+  | otherwise = x+n : xs
 
--- * Fcyvg n onfr10 rkcnafvba va n ghcyr pbagnvavat (yrsg,evtug) pbzcbaragf.
-_fcyvg :: [Vag] -> ([Vag],[Vag])
-_fcyvg kf 
-  | rira f'   = (\(y,e) -> (y,e))           $ Y.fcyvgNg (qvi f' 2) kf
-  | bgurejvfr = (\(y,e) -> (y++[urnq e],e)) $ Y.fcyvgNg (qvi f' 2) kf
-    jurer
-      f' = yratgu kf
+-- * Split a base10 expansion in a tuple containing (left,right) components.
+_split :: [Int] -> ([Int],[Int])
+_split xs 
+  | even s'   = (\(l,r) -> (l,r))           $ L.splitAt (div s' 2) xs
+  | otherwise = (\(l,r) -> (l++[head r],r)) $ L.splitAt (div s' 2) xs
+    where
+      s' = length xs
 
--- * Ergheaf gehr vs guvf vf gur ynfg cnyva bs guvf fvmr
-_ynfg_cnyvaqebzr []                 = Snyfr
-_ynfg_cnyvaqebzr [k]    | k==9      = Gehr
-_ynfg_cnyvaqebzr (k:kf) | k==9      = _ynfg_cnyvaqebzr kf
-                        | bgurejvfr = Snyfr
+-- * Returns true if this is the last palin of this size
+_last_palindrome []                 = False
+_last_palindrome [x]    | x==9      = True
+_last_palindrome (x:xs) | x==9      = _last_palindrome xs
+                        | otherwise = False
 
--- * Ergheaf gur arkg cnyvaqebzr
-arkg :: [Vag] -> [Vag]
-arkg kf  | yc'       = [1] ++ vavg m' ++ [1]
-         | ey'>e'    = y' ++ (erqhpr' ey')
-         | bgurejvfr = (erirefr ey1') ++ (erqhpr' ey1')
-  jurer
-    (y',ey',e',ey1') = (\(y,e) -> (y,erirefr y,e,ey' <+> 1)) $ _fcyvg kf
-    erqhpr' | rira (yratgu kf)   = vq
-            | bgurejvfr          = gnvy
-    yc' = _ynfg_cnyvaqebzr y'
-    m'  = znc (*0) kf
+-- * Returns the next palindrome
+next :: [Int] -> [Int]
+next xs  | lp'       = [1] ++ init z' ++ [1]
+         | rl'>r'    = l' ++ (reduce' rl')
+         | otherwise = (reverse rl1') ++ (reduce' rl1')
+  where
+    (l',rl',r',rl1') = (\(l,r) -> (l,reverse l,r,rl' <+> 1)) $ _split xs
+    reduce' | even (length xs)   = id
+            | otherwise          = tail
+    lp' = _last_palindrome l'
+    z'  = map (*0) xs
 
 
-{- vb -}
+{- io -}
 
-z_arkg = qb
-  yvar <- O.trgYvar
-  chgFgeYa $ znc P.vagGbQvtvg (arkg_cnyvaqebzr yvar)
-    jurer onfr10_rkcnafvba yvar = znc (\k -> sebzVagrteny(k-48)) (O.hacnpx yvar)
-          arkg_cnyvaqebzr  yvar = arkg (onfr10_rkcnafvba yvar)
+m_next = do
+  line <- B.getLine
+  putStrLn $ map C.intToDigit (next_palindrome line)
+    where base10_expansion line = map (\x -> fromIntegral(x-48)) (B.unpack line)
+          next_palindrome  line = next (base10_expansion line)
 
-z_ercrng a s | a>1       = s >> z_ercrng (a-1) s
-             | bgurejvfr = s
+m_repeat n f | n>1       = f >> m_repeat (n-1) f
+             | otherwise = f
 
-znva = qb
-  a <- trgYvar
-  z_ercrng (ernq a :: Vag) z_arkg
+main = do
+  n <- getLine
+  m_repeat (read n :: Int) m_next
